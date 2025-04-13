8aecd341b85faa16ef21669fbe5ae99cfa126d22
Anuj <anujtiwari4454@outlook.com>
2025-04-13 21:13:24 +0530

    add all avai core

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: node server.js
+worker: node --expose-gc server.js
--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 require("dotenv").config();
-const cluster = require("cluster");
-const os = require("os");
 const bot = require("./bot");
 const connectDB = require("./config/db");
 const sessionManager = require("./utils/sessionManager");
 const winston = require("winston");
-
-const numCPUs = os.cpus().length;
 
 const logger = winston.createLogger({
   level: "error",
@@ -20,53 +16,34 @@ const logger = winston.createLogger({
 
 process.on("unhandledRejection", (reason, promise) => {});
 
-if (cluster.isMaster) {
-  async function startBot() {
-    try {
-      await connectDB();
-      await sessionManager.initializeSessions();
-      await bot.launch();
-
-      process.once("SIGINT", () => bot.stop("SIGINT"));
-      process.once("SIGTERM", () => bot.stop("SIGTERM"));
-
-      if (process.env.NODE_ENV !== "production") {
-        console.log("Bot started in master process");
-      }
-    } catch (err) {
-      process.exit(1);
-    }
+try {
+  if (global.gc) {
+    setInterval(() => {
+      global.gc();
+    }, 30000);
   }
+} catch (e) {}
 
-  startBot();
+let memoryUsageLog = 0;
+setInterval(() => {
+  const memoryUsed = process.memoryUsage().heapUsed / 1024 / 1024;
 
-  for (let i = 0; i < numCPUs - 1; i++) {
-    cluster.fork();
+  if (Math.abs(memoryUsed - memoryUsageLog) > 20) {
+    memoryUsageLog = memoryUsed;
   }
+}, 60000);
 
-  cluster.on("exit", (worker, code, signal) => {
-    if (worker.process.pid !== process.pid) {
-      cluster.fork();
-    }
-  });
+async function startBot() {
+  try {
+    await connectDB();
+    await sessionManager.initializeSessions();
+    await bot.launch();
 
-  if (process.env.NODE_ENV !== "production") {
-    console.log(
-      `Master process running with ${numCPUs - 1} workers for background tasks`
-    );
+    process.once("SIGINT", () => bot.stop("SIGINT"));
+    process.once("SIGTERM", () => bot.stop("SIGTERM"));
+  } catch (err) {
+    process.exit(1);
   }
-} else {
-  async function startWorker() {
-    try {
-      await connectDB();
-
-      if (process.env.NODE_ENV !== "production") {
-        console.log(`Worker ${process.pid} started for background processing`);
-      }
-    } catch (err) {
-      process.exit(1);
-    }
-  }
-
-  startWorker();
 }
+
+startBot();
