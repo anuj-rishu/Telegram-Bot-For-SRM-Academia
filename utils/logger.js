52ef4a8e4683a8ec81beda448e77b1182fee1de9
Anuj <anujtiwari4454@outlook.com>
2025-09-01 22:13:46 +0530

    change in docker file

--- utils/logger.js
+++ utils/logger.js
@@ -1,23 +1,39 @@
-const pino = require("pino");
+const winston = require('winston');
 
-const logger = pino({
-  level: "info",
-  timestamp: pino.stdTimeFunctions.isoTime,
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      translateTime: "yyyy-mm-dd HH:MM:ss",
-      ignore: "pid,hostname",
-    },
-  },
+const customFormat = winston.format.combine(
+  winston.format.timestamp({
+    format: 'YYYY-MM-DD HH:mm:ss'
+  }),
+  winston.format.colorize(),
+  winston.format.printf(({ timestamp, level, message }) => {
+    return `[${timestamp}] ${level}: ${message}`;
+  })
+);
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: customFormat,
+  transports: [
+    new winston.transports.Console()
+  ],
+  exitOnError: false
 });
 
-process.on("uncaughtException", (err) =>
-  logger.error(`Uncaught Exception: ${err.stack || err}`)
-);
-process.on("unhandledRejection", (reason) =>
-  logger.error(`Unhandled Rejection: ${reason}`)
-);
+logger.api = function(method, endpoint, status, responseTime) {
+  this.info(`API ${method.toUpperCase()} ${endpoint} - Status: ${status} - ${responseTime}ms`);
+};
+
+logger.userActivity = function(userId, activity) {
+  this.info(`User ${userId}: ${activity}`);
+};
+
+process.on('uncaughtException', (error) => {
+  logger.error(`Uncaught Exception: ${error.message}`);
+  logger.error(error.stack);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(`Unhandled Rejection at Promise. Reason: ${reason}`);
+});
 
 module.exports = logger;
